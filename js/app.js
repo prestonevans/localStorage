e87642e32ee61aeb398f5d357fd2b0ab1454141c
Preston Evans <preston.evans3074@stu.mtec.edu>
2021-10-06 20:06:50 -0600

    finished!!!

--- js/app.js
+++ js/app.js
@@ -1,2 +1,38 @@
-//document.body.style.backgroundColor = "red";
-//test me
+const bidsStorageKey = 'arrayOfBids';
+const arrayOfBids = retrieve() || [];
+const btn = document.querySelectorAll('button');
+
+function render() {
+    const bids = document.querySelector('.bids');
+    bids.innerHTML = '';
+    for(let bid of arrayOfBids) {
+        const p = document.createElement('p');
+        p.innerText = `$ ${bid}`;
+        bids.prepend(p);
+    }
+}
+function save () {
+    localStorage.setItem(bidsStorageKey,JSON.stringify(arrayOfBids));
+}
+function retrieve() {
+    return JSON.parse(localStorage.getItem(bidsStorageKey));
+}
+for(let i = 1; i <= btn.length; i++) {
+    btn[i - 1].addEventListener('click', () => {
+        const input = document.querySelector(`input.bid${i}`);
+        if(!input.value) {
+            alert('enter something valid');
+            return;
+        } else if (parseInt(input.value) <= arrayOfBids[arrayOfBids.length - 1]) {
+            alert('You have been out bid...');
+            input.value = null;
+            return;
+        }
+        arrayOfBids.push(parseInt(input.value));
+        render();
+        save();
+        input.value = null;
+    })
+}
+render();
+save();
